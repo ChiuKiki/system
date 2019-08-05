7d04d017f94fbfc35c87fbb0f6fa3d0261484567
ChiuKiki <840084384@qq.com>
2019-08-05 21:14:39 +0800

    跨域4

--- app/Http/Middleware/CrossMiddleware.php
+++ app/Http/Middleware/CrossMiddleware.php
@@ -20,6 +20,6 @@ class CrossMiddleware
             ->header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, UPDATE, PATCH, DELETE')
             ->header('Access-Control-Allow-Headers', 'Content-Type, X-Session-Id, Cookie, multipart/form-data, application/json')
             ->header('Access-Control-Allow-Credentials', 'true')
-            ->header('Access-Control-Allow-Origin', '');
+            ->header('Access-Control-Allow-Origin', '*');
     }
 }
