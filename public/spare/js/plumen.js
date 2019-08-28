b5a230cb75132d8a25e1457f5ece38e5b72649a9
ChiuKiki <840084384@qq.com>
2019-08-28 22:22:09 +0800

    改样式3

--- public/spare/js/plumen.js
+++ public/spare/js/plumen.js
@@ -23,19 +23,29 @@
 
 					$(_this).append("<span class='wxSelect_bottom'></span>")
 					$(_this).find(".wxSelect_bottom").css({
+						"-webkit-appearance": "none",
+            "-moz-appearance": "none",
+            appearance: "none",
 						width:"8vw",
-						height:"6vw",
+						height:$(".wx-input").css("height"),
 						background:"rgb(155, 223, 128)",
 						display:"inline-block",
 						position:"absolute",
 						right:"0vw",
 						top:"0vw",
-						borderRadius:" 0 3vw 3vw 0 ",
+						transform: "translate(10%,3px)",
+	          "-o-transform": "translate(10%,3px)",
+	          "-ms-transform": "translate(10%,3px)",
+	          "-webkit-transform": "translate(0%,4px)",
+						borderRadius:" 0 30% 30% 0 ",
 						zIndex:2
 					})
 					$(_this).css({position:"relative",width:w + "px"}),
 					$(_this).append("<span class='wxSelect_label'></span>")
 					$(_this).find(".wxSelect_label").css({
+						"-webkit-appearance": "none",
+            "-moz-appearance": "none",
+            appearance: "none",
 						display: "inline-block",
 						width:"0",
 					    height:"0",
@@ -45,13 +55,16 @@
 						cursor: "pointer",
 						position: "absolute",
 						right: "2vw",
-						top: "2vw",
+						top: "40%",
 						borderRadius: '0',
 						display:"block",
 						zIndex:3
 					})
 					$(_this).append('<div class="dataBox"><ul class="dataList"></ul></div>');
 					$(_this).find(".dataBox").css({
+						"-webkit-appearance": "none",
+            "-moz-appearance": "none",
+            appearance: "none",
 						width: "100%",
 						maxHeight: "options.height" + "px",
 						overflowY: "scroll",
@@ -115,6 +128,9 @@
 						}
 						$(_this).find(".dataList").append(html);
 						$(_this).find(".dataList li").css({
+							"-webkit-appearance": "none",
+							"-moz-appearance": "none",
+							appearance: "none",
 							padding:"2vw 0 0 2vw",
 							border:"0",
 							listStyleType:"none",
