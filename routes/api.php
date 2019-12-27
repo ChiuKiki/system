ea9baf1466fbe4909b456543f7143ef6a59ec895
ChiuKiki <840084384@qq.com>
2019-12-27 16:45:39 +0800

    +测试文档

--- routes/api.php
+++ routes/api.php
@@ -17,40 +17,52 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//操作者登陆——登录
+/*
+ * Login Module
+ */
+//登录
 Route::any('operatorLogin','PeopleController@loginModel');
-//添加人员——注册
+//注册
 Route::any('insert','PeopleController@insertModel');
 //忘记密码
 Route::any('forgetPassword','PeopleController@forgetPasswordModel');
-//操作者注销
+//注销
 Route::any('operatorLogout','PeopleController@logoutModel');
 
-
-//显示所有人员——百步梯通讯录
+/*
+ * Address Book Module
+ */
+//显示所有人员
 Route::any('queryInitial','PeopleController@queryInitialModel')->middleware('checkLoginMiddleware');
-//搜索框查询1——百步梯通讯录
+//搜索框
 Route::any('queryInfo','PeopleController@queryInfoModel')->middleware('checkLoginMiddleware');
-//搜索框查询2——百步梯通讯录（修改状态）
-Route::any('queryInfoAdmin','PeopleController@queryInfoAdminModel')->middleware('checkAdminMiddleware');
-//管理员获取他人信息——百步梯通讯录（修改状态）
-Route::any('queryAdmin','PeopleController@queryAdminModel')->middleware('checkAdminMiddleware');
-//批量删除人员——百步梯通讯录（修改状态）
-Route::any('delete','PeopleController@deleteModel')->middleware('checkAdminMiddleware');
-//修改人员——百步梯通讯录（修改状态）
-Route::any('update','PeopleController@updateAdminModel');
-
-
-//点击某人名字查询其信息——详细信息
+//点击某人名字查询其信息
 Route::any('query','PeopleController@queryModel')->middleware('checkLoginMiddleware');
-//获取自己信息——个人信息
+
+/*
+ * Personal Information Module
+ */
+//获取自己信息
 Route::any('queryNumber','PeopleController@queryNumberModel')->middleware('checkLoginMiddleware');
-//自己修改自己信息——个人信息
+//修改自己信息
 Route::any('updatePeople','PeopleController@updatePeopleModel');
 
+/*
+ * Administrator Module
+ */
+//搜索框
+Route::any('queryInfoAdmin','PeopleController@queryInfoAdminModel')->middleware('checkAdminMiddleware');
+//获取他人信息
+Route::any('queryAdmin','PeopleController@queryAdminModel')->middleware('checkAdminMiddleware');
+//批量删除人员
+Route::any('delete','PeopleController@deleteModel')->middleware('checkAdminMiddleware');
+//修改人员
+Route::any('update','PeopleController@updateAdminModel');
 
+/*
+ * Timetable Module
+ */
 //没课表查询
 Route::any('free','TimetableController@noClassModel')->middleware('checkLoginMiddleware');
 //没课表录入
 Route::any('insertFree','TimetableController@insertNoClassModel')->middleware('checkLoginMiddleware');
-
